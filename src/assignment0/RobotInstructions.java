ce4fd1b195995bffe52136e1c15402885adb3294
CAverbeck <CAverbeck@DESKTOP-5K5U3I7.dhcp.wustl.edu>
2025-01-21 20:39:36 -0600

    made a leaf :)

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,31 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
 		robot.turnRight(90);
+		robot.forward(33);
+		robot.turnLeft(45);
+		robot.forward(120);
+		robot.forward(10);
+		robot.turnLeft(135);
 		robot.forward(80);
+		robot.turnRight(110);
+		robot.forward(60);
+		robot.turnLeft(65);
+		robot.forward(125);
+		robot.turnLeft(90);
+		robot.forward(128);
+		robot.turnLeft(65);
+		robot.forward(58);
+		robot.turnRight(109);
+		robot.forward(75);
+		robot.turnLeft(135);
+		robot.forward(130);
+		robot.turnLeft(45);
+		robot.forward(50);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(50);
 	}
 
 }
